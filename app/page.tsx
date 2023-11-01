3bf8dd8355225d37ee2e5a4a505aa5a008d651b2
Kevin Papamarcos <kevin.papamarcos@gmail.com>
2023-11-01 18:38:16 -0500

    11/1/23

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,65 @@
 // app/providers.tsx
 'use client'
 
-import { Card, CardHeader, CardBody, CardFooter, Button } from '@chakra-ui/react'
+import { Card, CardHeader, CardBody, CardFooter, Button, Heading, Text, SimpleGrid, Input, Select } from '@chakra-ui/react'
+import { useState, useEffect} from 'react'
 
-async function api<T>(url: string) {
-  const params = {
-    method: 'GET',
-    headers: {
-        'accept': 'application/json'
-    }
-};
 
-  var ret = await fetch('http://localhost:8000/api/recipes/all', params)
-  .then(response => response.json())
-  .then(json => json)
-  .catch(e => console.error(e));
-  return ret;
+function CardFromJson(props) {
+  return (
+    <Card>
+    <CardHeader>
+      <Heading size='md'>{props.dish_name}</Heading>
+    </CardHeader>
+    <CardBody>
+      <Text>{props.dish_cuisine}</Text>
+    </CardBody>
+    <CardFooter>
+      <Button>{props.dish_id}</Button>
+    </CardFooter>
+  </Card>
+  )
 }
 
-function BasicCard(props) {
-  var json_response = api("http://localhost:8000/api/recipes/all");
-  return json_response.then(json => {
-    console.log(json);
-    return (
-      <Card>
-      <CardHeader>
-        {json[0].dish_name}
-      </CardHeader>
-      <CardBody>
-        {json[0].dish_cuisine}
-      </CardBody>
-    </Card>
-    )
-  })
+function RecipeCardArray() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/recipes/all')
+      .then(response => response.json())
+      .then(json => {
+        setData(json);
+        console.log(data);
+      })
+  }, []);
+  return (
+    <>
+    <SimpleGrid columns = {4} spacing='40px' margin='40px'>
+    {data.map(value => <CardFromJson dish_name = {value.dish_name} dish_cuisine = {value.dish_cuisine} dish_id = {value.dish_id} ></CardFromJson>)}
+    </SimpleGrid>
+    </>
+  )
 }
 
+function TopHeader() {
+  return (
+    <div>
+      <SimpleGrid columns={2} spacing='40px' margin='40px'>
+      <Input placeholder='medium size' size='med' />
+      <Select placeholder='Select option'>
+      <option value='option1'>Option 1</option>
+      <option value='option2'>Option 2</option>
+      <option value='option3'>Option 3</option>
+      </Select>
+      </SimpleGrid>
+    </div>
+  )
+}
 
 export default function App() {
   return (
     <>
-    <BasicCard name={"name1"}/>
+    <TopHeader></TopHeader>
+    <RecipeCardArray></RecipeCardArray>
     </>
   )
 }
