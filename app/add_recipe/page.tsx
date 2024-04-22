e353785aeccf1f33600ad3bcdfbd2fa93c29e2b2
Kevin Papamarcos <kevin.papamarcos@gmail.com>
2024-04-21 19:49:03 -0500

    updates 4/21/24

--- app/add_recipe/page.tsx
+++ app/add_recipe/page.tsx
@@ -10,6 +10,8 @@ import {
   NumberInputStepper,
   NumberIncrementStepper,
   NumberDecrementStepper,
+  Select,
+  Button,
   Heading,
   Center
 } from '@chakra-ui/react'
@@ -17,11 +19,28 @@ import { useState, useEffect} from 'react'
 import {AppMenu} from '../menu'
 import {Header} from '../header'
 
+function TopHeader() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/cuisines/all')
+      .then(response => response.json())
+      .then(json => {
+        setData(json);
+      })
+  }, []);
+  return (
+    <Select placeholder="Cuisine">
+      <option value="All">{"All"}</option>
+      {data.map(cuisine => <option value= {cuisine.dish_cuisine} >{cuisine.dish_cuisine}</option>)}
+    </Select>
+  )
+}
 
-function RecipeIngredient(props) {
+
+function RecipeIngredient() {
   return (
     <Box>
-      <SimpleGrid columns={3}>
+      <SimpleGrid columns={3} spacing='40px' margin='40px'>
         <Input placeholder="Ingredient Name"></Input>
         <Input placeholder="Quantity Type"></Input>
         <NumberInput>
@@ -36,11 +55,30 @@ function RecipeIngredient(props) {
   )
 }
 
+function IngredientWrapper(props) {
+  const [ingredients, setIngredients] = useState<typeof RecipeIngredient[]>([]);
+  function addIngredient() {
+    const nextIngredients : any[] = ingredients.slice();
+    nextIngredients.push(RecipeIngredient());
+    setIngredients(nextIngredients);
+  }
+  return (
+    <div>
+      {ingredients}
+      <Button onClick={addIngredient}>Add Ingredient</Button>
+    </div>
+  )
+}
+
 export default function AddRecipe() {
   return (
     <div>
       <Header text="Add A Recipe"></Header>
-      <RecipeIngredient></RecipeIngredient>
+      <SimpleGrid columns={2} spacing='40px' margin='40px'>
+        <Input placeholder="Recipe Name"></Input>
+        <TopHeader></TopHeader>
+      </SimpleGrid>
+      <IngredientWrapper></IngredientWrapper>
     </div>
   )
 }
